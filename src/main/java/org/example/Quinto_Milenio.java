25131e871f0f7f145165b85dd4aebfc8da28b137
A705\JorgeC.G. <jorgecuartero29@gmail.com>
2025-12-11 17:29:25 +0100

    no message

---
+++ src/main/java/org/example/Quinto_Milenio.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Quinto_Milenio {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Cantidad de frases que vas a poner: ");
+        int cant = entrada.nextInt();
+
+
+        System.out.println("Introduce la cadena o la frase: ");
+        String cadena = entrada.nextLine();
+
+
+        String odio = "te odio";
+
+        for (int i = 0; i < cant; i++) {
+
+
+        }
+    }
+}
